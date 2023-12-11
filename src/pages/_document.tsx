ce23b4d32b017d49b4da3de8289c6491f2433602
Dev Khandelwal <122960451+khandelwaldev@users.noreply.github.com>
2023-12-11 16:24:39 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en-IN" >
+    <Html lang="en-IN" className="scroll-smooth">
       <Head />
       <body className="bg-[#010101] font-default text-white">
         <Main />
